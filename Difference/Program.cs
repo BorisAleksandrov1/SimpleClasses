8f0429cb78638273cf8eadf07fe7eda3ed7d96f2
BoriskataMBT <bobialeksandrov5@gmail.com>
2025-02-24 09:48:39 +0200

    Added first ex answear

---
+++ Difference/Program.cs
@@ -0,0 +1,24 @@
+﻿class Program
+{
+    public static void Main()
+    {
+        string dateString1 = Console.ReadLine();
+        string dateString2 = Console.ReadLine();
+
+        DateDiff.FindDIff(dateString1, dateString2);
+    }
+}
+
+class DateDiff
+{
+    public static void FindDIff(string dateString1, string dateString2)
+    {
+        DateTime date1 = DateTime.ParseExact(dateString1, "yyyy mm dd", System.Globalization.CultureInfo.InvariantCulture);
+        DateTime date2 = DateTime.ParseExact(dateString2, "yyyy mm dd", System.Globalization.CultureInfo.InvariantCulture);
+
+        TimeSpan difference = date1 - date2;
+        int result = Math.Abs(difference.Days);
+
+        Console.WriteLine(result);
+    }
+}
